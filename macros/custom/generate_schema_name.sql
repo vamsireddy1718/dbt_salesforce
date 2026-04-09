b4a91abaaedb146cffbd5886c8bcf80bbe0a9229
vamsireddy1718 <VENNA.REDDY@INSIGHTSOFTWARE.COM>
2026-04-09 06:02:12 +0000

    Add schema separation and clean schema names

--- macros/custom/generate_schema_name.sql
+++ macros/custom/generate_schema_name.sql
@@ -1,19 +1,9 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
-    {%- set default_schema = target.schema -%}
-
-    {%- if target.name == 'prod' -%}
-        {%- if custom_schema_name is not none -%}
-            {{ custom_schema_name | trim }}
-        {%- else -%}
-            {{ default_schema }}
-        {%- endif -%}
+    {%- if custom_schema_name is not none -%}
+        {{ custom_schema_name | trim }}
     {%- else -%}
-        {%- if custom_schema_name is not none -%}
-            {{ default_schema }}_{{ custom_schema_name | trim }}
-        {%- else -%}
-            {{ default_schema }}
-        {%- endif -%}
+        {{ target.schema }}
     {%- endif -%}
 
 {%- endmacro %}
